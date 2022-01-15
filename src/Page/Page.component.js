a5a867a445a2b9bdb6654e9e6756e6e7d4701c01
Chrysellia <priscilliachryso@gmail.com>
2022-01-15 14:47:53 +0300

    Page component

---
+++ src/Page/Page.component.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Components/Header/Header.component'
+import About from './Components/About/About.component'
+
+const Page = () => {
+    return (
+        <div>
+            <Header/>
+            <About/>
+        </div>
+    )
+}
+
+export default Page
