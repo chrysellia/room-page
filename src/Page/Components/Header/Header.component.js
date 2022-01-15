c986520833d0ecde7e0cb5c3dd9d3789ec475496
Chrysellia <priscilliachryso@gmail.com>
2022-01-15 18:45:37 +0300

    Header mobile tried

--- src/Page/Components/Header/Header.component.js
+++ src/Page/Components/Header/Header.component.js
@@ -4,43 +4,87 @@ import './Header.style.scss'
 const Header = () => {
     return (
         <div className="header-page">
-            <div className="header-wrapper" style={{backgroundImage:'url(images/desktop-image-hero-1.jpg)'}}>
-                <div className="header-navbar">
-                    <div className="navbar-logo">room</div>
-                    <div className="navbar-menu">home</div>
-                    <div className="navbar-menu">shop</div>
-                    <div className="navbar-menu">about</div>
-                    <div className="navbar-menu">contact</div>
+            <div className="desktop-design">
+                <div className="header-wrapper" style={{backgroundImage:'url(images/desktop-image-hero-1.jpg)'}}>
+                    <div className="header-navbar">
+                        <div className="navbar-logo">room</div>
+                        <div className="navbar-menu">home</div>
+                        <div className="navbar-menu">shop</div>
+                        <div className="navbar-menu">about</div>
+                        <div className="navbar-menu">contact</div>
+                    </div>
                 </div>
-            </div>
 
-            <div className="home-page">
-                <div className="home-wrapper">
-                    <div className="home-content">
-                        <div className="home-title">Discover innovative <br/> ways to decorate</div>
-                        <div className="home-description">
-                            <p>
-                                We provide unmateched quality and style for property <br/>
-                                owners accross the country. Our experts comine form and <br/>
-                                function in bringing your vision to life. Create a room in yoir own <br/>
-                                style with our collection and male your property a reflection of <br/>
-                                you and what you love.
-                            </p>
-                        </div>
-                        <div className="home-button">
-                            <div className="button-title">SHOP NOW</div>
-                            <div className="button-arrow">
-                                <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero"/></svg>
+                <div className="home-page">
+                    <div className="home-wrapper">
+                        <div className="home-content">
+                            <div className="home-title">Discover innovative <br/> ways to decorate</div>
+                            <div className="home-description">
+                                <p>
+                                    We provide unmateched quality and style for property <br/>
+                                    owners accross the country. Our experts comine form and <br/>
+                                    function in bringing your vision to life. Create a room in yoir own <br/>
+                                    style with our collection and male your property a reflection of <br/>
+                                    you and what you love.
+                                </p>
+                            </div>
+                            <div className="home-button">
+                                <div className="button-title">SHOP NOW</div>
+                                <div className="button-arrow">
+                                    <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero"/></svg>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="button-slider">
-                    <div className="button-wrapper">
-                        <img className="slider-item" src="images/icon-angle-left.svg"/>
+                    <div className="button-slider">
+                        <div className="button-wrapper">
+                            <img className="slider-item" src="images/icon-angle-left.svg"/>
+                        </div>
+                        <div className="button-wrapper">
+                            <img className="second-item" src="images/icon-angle-right.svg"/>
+                        </div>
                     </div>
-                    <div className="button-wrapper">
-                        <img className="second-item" src="images/icon-angle-right.svg"/>
+                </div>
+            </div>
+
+            <div className="mobile-design">
+                <div className="header-wrapper" style={{backgroundImage:'url(images/desktop-image-hero-1.jpg)'}}>
+                    <div className="header-navbar">
+                        <div className="navbar-hamburger">
+                            <svg width="20" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M20 12v2H0v-2h20zm0-6v2H0V6h20zm0-6v2H0V0h20z" fill="#FFF" fill-rule="evenodd"/></svg>
+                        </div>
+                        <div className="navbar-logo">room</div>
+                    </div>
+                    <div className="button-slider">
+                        <div className="button-wrapper">
+                            <img className="slider-item" src="images/icon-angle-left.svg"/>
+                        </div>
+                        <div className="button-wrapper">
+                            <img className="second-item" src="images/icon-angle-right.svg"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="home-page">
+                    <div className="home-wrapper">
+                        <div className="home-content">
+                            <div className="home-title">Discover innovative <br/> ways to decorate</div>
+                            <div className="home-description">
+                                <p>
+                                    We provide unmateched quality and style for property <br/>
+                                    owners accross the country. Our experts comine form and <br/>
+                                    function in bringing your vision to life. Create a room in yoir own <br/>
+                                    style with our collection and male your property a reflection of <br/>
+                                    you and what you love.
+                                </p>
+                            </div>
+                            <div className="home-button">
+                                <div className="button-title">SHOP NOW</div>
+                                <div className="button-arrow">
+                                    <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero"/></svg>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
